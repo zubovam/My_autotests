e0d37e2f46fa6196809b0751d7024f4d3f8509c5
zubovam <zubovam@gmail.com>
2023-04-15 22:24:27 +0300

    Add data.py, generator.py, TestCheckBox, CheckBoxPage, CheckBoxPageLocators,

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,8 +9,21 @@ class TestElements:
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_field()
             out_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_field()
-            # time.sleep(3)
+            # time.sleep(5)
             assert full_name == out_name
             assert email == out_email
             assert current_address == out_current_address
             assert permanent_address == out_permanent_address
+
+
+    class TestCheckBox:
+
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random()
+            input_checkbox = check_box_page.get_checked_box()
+            output_checkbox = check_box_page.get_output_result()
+            # time.sleep(5)
+            assert input_checkbox == output_checkbox
